9b2e53cd9c128122f2f30492dca1594e87a38538
Laura <milanbartky@gmail.com>
2023-04-20 18:14:45 +0200

    Initial functionality

---
+++ extension.js
@@ -0,0 +1,118 @@
+const vscode = require('vscode'),
+	fetch = require('node-fetch');
+
+const { createNativeObject, createNativeDocumentation, formatParameters, formatReturns, getPositionContext } = require('./natives.js');
+
+const natives = [];
+
+async function getJSON(url) {
+	const response = await fetch(url);
+
+	return response.json();
+}
+
+async function fetchNatives(url) {
+	const json = await getJSON(url);
+
+	if (!json) {
+		console.log('Failed to fetch natives from ' + url);
+
+		return;
+	}
+
+	for (const category in json) {
+		const children = json[category];
+
+		for (const child in children) {
+			const data = children[child];
+
+			natives.push(createNativeObject(data));
+		}
+	}
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	if (natives.length > 0) return;
+
+	vscode.window.withProgress({
+		location: vscode.ProgressLocation.Window,
+		cancellable: false,
+		title: 'Fetching Natives'
+	}, async (progress) => {
+		progress.report({ increment: 10 });
+
+		await fetchNatives('https://runtime.fivem.net/doc/natives_cfx.json');
+		await fetchNatives('https://runtime.fivem.net/doc/natives.json');
+
+		console.log('Fetched ' + natives.length + ' natives.');
+
+		progress.report({ increment: 100 });
+	});
+
+	// completion provider
+	const completionDisposable = vscode.languages.registerCompletionItemProvider('lua', {
+		provideCompletionItems(document, position) {
+			const ctx = getPositionContext(document, position);
+
+			const items = natives.filter(native => {
+				return native.name.startsWith(ctx.name);
+			}).map(native => {
+				const item = new vscode.CompletionItem(native.name, vscode.CompletionItemKind.Function);
+
+				item.detail = native.detail;
+				item.documentation = native.description;
+
+				item.sortText = (native.apiset === ctx.ctx ? '' : '~') + native.name;
+
+				let insertText = native.name + '(' + formatParameters(native.params, true) + ')';
+
+				if (!ctx.inline) {
+					insertText = formatReturns(native.returns, true) + insertText;
+				}
+
+				item.insertText = new vscode.SnippetString(insertText);
+
+				return item;
+			});
+
+			return items;
+		}
+	});
+
+	// hover provider
+	const hoverDisposable = vscode.languages.registerHoverProvider('lua', {
+		provideHover(document, position) {
+			const ctx = getPositionContext(document, position);
+
+			if (!ctx.name) return;
+
+			let native = natives.find(native => {
+				return native.name === ctx.name && native.apiset === ctx.ctx;
+			});
+
+			if (!native) {
+				native = natives.find(native => {
+					return native.name === ctx.name;
+				});
+
+				if (!native) return;
+			}
+
+			return new vscode.Hover(createNativeDocumentation(native));
+		}
+	});
+
+	context.subscriptions.push(completionDisposable);
+	context.subscriptions.push(hoverDisposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
---
+++ natives.js
@@ -0,0 +1,189 @@
+const underscoreParts = [
+	'3d',
+	'2d',
+	'2'
+];
+
+// Converts the native type to a Lua-friendly type
+function createLuaType(type) {
+	switch (type) {
+		case 'Vector4':
+		case 'Vector3':
+		case 'Vector2':
+			return type.toLowerCase();
+		case 'float':
+			return 'number';
+		case 'int':
+			return 'integer';
+		case 'BOOL':
+			return 'boolean';
+		case 'char':
+		case 'char*':
+			return 'string';
+	}
+
+	return type;
+}
+
+// Converts the native name to a Lua-friendly name
+function createNativeLuaName(name) {
+	const parts = name.split('_');
+
+	return parts.map(part => {
+		part = part.toLowerCase();
+
+		if (underscoreParts.includes(part)) {
+			return '_' + part;
+		}
+
+		return part.charAt(0).toUpperCase() + part.slice(1);
+	}).join('');
+}
+
+// Generates the hover documentation for a native
+function createNativeDocumentation(native) {
+	let lines = [
+		`Namespace: **${native.ns}** - API set: **${native.apiset}**`,
+		`[${native.hash}](https://docs.fivem.net/natives/?_${native.hash})`
+	];
+
+	const params = native.params.length > 0 ? '\n' + formatParameters(native.params, false, '    ', ',\n') + '\n' : '';
+
+	const returns = native.returns.length > 0 ? formatReturns(native.returns) : '';
+
+	const documentation = [
+		`${returns}${native.name}(${params})`
+	];
+
+	if (native.original) {
+		documentation.unshift(`-- ${native.original}`);
+	}
+
+	lines.push('```lua\n' + documentation.join('\n') + '\n```');
+
+	lines.push('```\n' + native.description + '\n```');
+
+	return lines;
+}
+
+// Cleans up the servers json
+function createNativeObject(data) {
+	const apiset = data.apiset || 'client';
+
+	const returns = data.params.filter(param => {
+		return param.type.endsWith('*') && param.type !== 'char*';
+	}).map(param => {
+		return {
+			name: param.name,
+			type: createLuaType(param.type.replace('*', ''))
+		};
+	});
+
+	if (data.results) {
+		returns.unshift({
+			name: 'retval',
+			type: createLuaType(data.results)
+		});
+	}
+
+	const params = data.params.filter(param => {
+		return !param.type.endsWith('*') || param.type === 'char*';
+	}).map(param => {
+		return {
+			name: param.name,
+			type: createLuaType(param.type.replace('*', ''))
+		};
+	});
+
+	const detail = apiset + ': ' + (returns.length === 0 ? 'void' : returns.map(ret => ret.type).join(', '));
+
+	return {
+		hash: data.hash,
+		original: data.name,
+		name: data.name ? createNativeLuaName(data.name) : data.hash,
+		params: params,
+		returns: returns,
+		detail: detail,
+		description: data.description,
+		ns: data.ns,
+		apiset: apiset
+	};
+}
+
+// Gets the context of the current position (client or server)
+function getFileContext(filename) {
+	if (filename.includes('server') || filename.includes('sv_')) {
+		return 'server';
+	}
+
+	return 'client';
+}
+
+// Formats the parameters
+function formatParameters(params, plain, indent, join) {
+	indent = indent || '';
+
+	return params.map(param => {
+		const name = 'p' + param.name.charAt(0).toUpperCase() + param.name.slice(1);
+
+		return indent + name + (plain ? '' : ' --[[ ' + param.type + ' ]]');
+	}).join(join || ', ');
+}
+
+// Formats the return values
+function formatReturns(returns, plain) {
+	return 'local ' + returns.map(ret => {
+		return (plain ? '' : '--[[ ' + ret.type + ' ]] ') + ret.name;
+	}).join(', ') + ' = ';
+}
+
+function getPositionContext(document, position) {
+	let previousLine = position.line > 0 ? document.lineAt(position.line - 1).text : '',
+		line = document.lineAt(position.line).text;
+
+	let linePrefix = line.substring(0, position.character),
+		lineSuffix = line.substring(position.character);
+
+	const prefixMatch = linePrefix.match(/(\w+)$/),
+		suffixMatch = lineSuffix.match(/^(\w+)/);
+
+	const prefix = prefixMatch ? prefixMatch[0] : '',
+		suffix = suffixMatch ? suffixMatch[0] : '';
+
+	const functionName = prefix + suffix,
+		context = getFileContext(document.fileName);
+
+	linePrefix = linePrefix.substring(0, linePrefix.length - prefix.length).trim();
+
+	if (linePrefix.endsWith(',') || linePrefix.endsWith('(') || linePrefix.endsWith('=')) {
+		return {
+			inline: true,
+			name: functionName,
+			ctx: context
+		};
+	}
+
+	previousLine = previousLine.trim();
+
+	if (previousLine.endsWith(',')) {
+		return {
+			inline: true,
+			name: functionName,
+			ctx: context
+		};
+	}
+
+	return {
+		inline: false,
+		name: functionName,
+		ctx: context
+	};
+}
+
+module.exports = {
+	createNativeObject,
+	formatParameters,
+	formatReturns,
+	getPositionContext,
+	createNativeDocumentation
+}
